c1b9bfb3ade8e4dde204dba53f4cbb6f6c4ea91f
Gab <59850744+G40b@users.noreply.github.com>
2024-01-14 20:22:19 -0300

    Customize logger

---
+++ src/main/java/util/JafLogger.java
@@ -0,0 +1,31 @@
+package util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class JafLogger {
+    public static final String GREEN  = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String WHITE  = "\u001B[37m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String RED = "\u001B[31m";
+    public static final String RESET  = "\u001B[0m";
+
+    public static DateTimeFormatter DATE = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+
+    public static void showBanner() {
+        System.out.println(BLUE);
+        System.out.println("  ______          _____    ______   ______  ");
+        System.out.println(" / / / /         / /   |  / ____/   \\ \\ \\ \\ ");
+        System.out.println("/ / / /     __  / / /| | / /_        \\ \\ \\ \\  Jaf's a framework (Jaf)");
+        System.out.println("\\ \\ \\ \\    / /_/ / ___ |/ __/        / / / /  Java Framework");
+        System.out.println(" \\_\\_\\_\\   \\____/_/  |_/_/          /_/_/_/   Minimalist Web Framework");
+        System.out.println(RESET);
+    }
+    public static void log(String modulo, String mensagem) {
+        String date = LocalDateTime.now().format(DATE);
+        System.out.printf(WHITE+"%15s "+RED+"%-30s:"+WHITE+"%s\n"+RESET, date, modulo, mensagem);
+    }
+
+}
--- src/main/java/web/JafApplication.java
+++ src/main/java/web/JafApplication.java
@@ -3,12 +3,21 @@ package web;
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
+import util.JafLogger;
 
 import java.io.File;
 
 public class JafApplication {
     public static void run() {
+
+        // zera o log do apache tomcat
+        java.util.logging.Logger.getLogger("org.apache").setLevel(java.util.logging.Level.OFF);
+        long ini, fim;
+        JafLogger.showBanner();
+
         try {
+            ini = System.currentTimeMillis();
+            JafLogger.log("Embeded Web Container", "Starting (...)");
 
             Tomcat tomcat = new Tomcat();
             Connector connector = new Connector();
@@ -22,6 +31,8 @@ public class JafApplication {
             context.addServletMappingDecoded("/*", "JafDispatchServlet");
 
             tomcat.start();
+            fim = System.currentTimeMillis();
+            JafLogger.log("Embeded Web Container", "started in " + ((double) (fim - ini) / 1000) + " seconds");
             tomcat.getServer().await();
 
         } catch (Exception e) {
